28220fc0687cbb5adc8a197eb5d6744828589468
miguel chirino <mchirinot@gmail.com>
2020-11-03 18:29:51 +0100

    photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,12 @@
 class Photo < ApplicationRecord
+
+    CATEGORIES = ['Nature', 'Water Sports','Places','Other'].freeze
+    has_one_attached :photo
+    belongs_to :user
+
+    pg_search_scope :search_by_category,
+    against: [:category],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
